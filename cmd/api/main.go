29fa46dc8a32c7b269d76b76194a1b56da50df16
Vasilev Saveliy <sadfqwder@gmail.com>
2025-06-25 12:45:38 +0300

    add async smtp messaging & background tasks helper func

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"log/slog"
 	"os"
+	"sync"
 	"time"
 
 	"github.com/joho/godotenv"
@@ -46,6 +47,7 @@ type application struct {
 	logger *slog.Logger
 	models data.Models
 	mailer mailer.Mailer
+	wg     sync.WaitGroup
 }
 
 func main() {
